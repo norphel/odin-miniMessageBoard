db7f731a885781f8cade6e4cc81c80e757a8882b
Norphel <113838216+norphel@users.noreply.github.com>
2024-03-10 17:04:34 +0530

    fix bugs and add index file

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 import express from "express";
-import path from "path";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", "src/views");
 app.set("view engine", "pug");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 // routes import
 import indexRouter from "./routes/index.route.js";
